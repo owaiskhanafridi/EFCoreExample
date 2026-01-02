4440994ed7659ff3a9918f4371ca808bc220cf25
Khan, Owais <A0076323@kiongroup.com>
2026-01-02 11:13:02 -0500

    Added Xunit project following youtube video tutorial: https://www.youtube.com/watch?v=GMYCNfDXQIk&list=PL82C6-O4XrHeyeJcI5xrywgpfbrqdkQd4&index=5

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,117 @@
+﻿using FluentAssertions;
+using FluentAssertions.Extensions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        private readonly NetworkService _service;
+
+        public NetworkServiceTests()
+        {
+            _service = new NetworkService();
+        }
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            //Arrange
+            //var pingService = new NetworkService();
+
+            //Act
+            var result = _service.SendPing();
+
+            // Assert
+            // Use the FluentAssertion website to check all kind of assertions:
+            // https://fluentassertions.com/introduction
+            
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent!");
+            result.Should().Contain("Success", Exactly.Once());
+
+        }
+
+        //Theory is used when the unit test function need to receive params
+        //Inline is used as an input and return value
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(2,4,6)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            //Arrange 
+            //var pingService = new NetworkService();
+            
+            //Act
+            var result = _service.PingTimeout(a, b);
+
+            //Assert
+            result.Should().Be(expected);
+            result.Should().BeGreaterThanOrEqualTo(2);
+            result.Should().NotBeInRange(-10000, 0);
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnDateTime()
+        {
+            //Arrange
+            var pingService = new NetworkService();
+
+            //Act
+            var result = _service.LastPingDate();
+
+            // Assert
+            // Use the FluentAssertion website to check all kind of assertions:
+            // https://fluentassertions.com/introduction
+
+            result.Should().BeAfter(3.January(2010));
+            result.Should().BeBefore(3.December(2028));
+
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnsObject()
+        {
+            //Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _service.GetPingOptions();
+
+            // Assert: WARNING: Use BeEquivalent when comparing object. string and int works without Equivalent though like previous methods.
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnsObject()
+        {
+            //Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            //Act
+            var result = _service.MostRecentPings();
+
+            // Assert: WARNING: Use BeEquivalent when comparing object. string and int works without Equivalent though like previous methods.
+            //result.Should().BeOfType<IEnumerable<PingOptions>>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        => "Success: Ping Sent!";
+
+        public int PingTimeout(int a, int b)
+         => a + b;
+
+        public DateTime LastPingDate()
+            => DateTime.Now;
+
+        public PingOptions GetPingOptions()
+            => new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 2
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 3
+                },
+            };
+
+            return pingOptions;
+        } 
+    }
+}
